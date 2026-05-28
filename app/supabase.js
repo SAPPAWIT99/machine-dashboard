4ca2c11a5df6d5165993f4baa2e209a160165e03
SAPPAWIT99 <sapawit@hotmail.com>
2026-05-28 19:59:26 +0700

    fix supabase connection

--- app/supabase.js
+++ app/supabase.js
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://gsrhseuwcmckjztvaoui.supabase.co";
 
-const supabaseKey = "วาง Publishable Key ของคุณ";
+const supabaseKey = "sb_publishable_3u5aI1so-gYBmsHhJjQrPw_J3Caf8GB";
 
 export const supabase = createClient(
   supabaseUrl,
